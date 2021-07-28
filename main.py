53d236570c6b70c818b422ad3571c2fa8332b5e2
Abraham George Smith <Abe404@users.noreply.github.com>
2021-07-28 14:25:24 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021 Abraham Smith
+# dicom_mask is released under a BSD license and contains code copied/modified from dicompyer.
+# See license: https://github.com/Abe404/dicom_mask/LICENSE
+
+# dicompyler is released under a BSD license.
+# The dicomplyer code is Copyright (c) 2009-2017 Aditya Panchal
+#    See the file license.txt
+#    available at https://github.com/bastula/dicompyler/

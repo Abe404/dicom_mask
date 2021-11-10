69ce70988069e4a559a881fc8ea4d8556e3d1084
Abraham George Smith <abe@abesmith.co.uk>
2021-11-10 16:47:21 +0100

    simplify requirements (dont use dev version), reply setup.cfg with setup.py which now fixes the issue with installing the package not installing dependencies, also add publish.sh to simplify publish process

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+from pathlib import Path
+
+current_dir = Path(__file__).parent
+long_description = (current_dir / "README.md").read_text()
+
+setup(
+  name = 'dicom_mask',
+  packages = ['dicom_mask'],
+  version = '0.0.16',
+  license = 'BSD', 
+  description = 'Export structure form dicom to numpy binary mask',
+  long_description_content_type='text/markdown',
+  long_description=long_description,
+  author = 'Abraham George Smith',
+  author_email = 'abe@abesmith.co.uk',
+  url = 'https://github.com/Abe404/dicom_mask',
+  download_url = 'https://github.com/Abe404/dicom_mask/archive/refs/tags/0.0.16.tar.gz',
+  keywords = ['DICOM', 'NUMPY', 'RTSRUCT'],
+  install_requires=[
+      "dicompyler-core ==0.5.5",
+      "numpy ==1.21.0",
+      "pydicom ==2.2.0",
+      "scikit-image ==0.18.2"
+  ],
+  classifiers=[
+    'Intended Audience :: Developers',
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent'
+  ]
+)

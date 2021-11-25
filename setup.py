beab06f66f200b22b46d9864262faa55fcfb1a04
Abraham George Smith <abe@abesmith.co.uk>
2021-11-25 15:05:47 +0100

    fix description

--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ long_description = (current_dir / "README.md").read_text()
 setup(
   name = 'dicom_mask',
   packages = ['dicom_mask'],
-  version = '0.0.18',
+  version = '0.0.19',
   license = 'BSD', 
-  description = 'Export structure form dicom to numpy binary mask',
+  description = 'Export structure from dicom to numpy binary mask',
   long_description_content_type='text/markdown',
   long_description=long_description,
   author = 'Abraham George Smith',
   author_email = 'abe@abesmith.co.uk',
   url = 'https://github.com/Abe404/dicom_mask',
-  download_url = 'https://github.com/Abe404/dicom_mask/archive/refs/tags/0.0.18.tar.gz',
+  download_url = 'https://github.com/Abe404/dicom_mask/archive/refs/tags/0.0.17b.tar.gz',
   keywords = ['DICOM', 'NUMPY', 'RTSRUCT', 'MASK'],
   install_requires=[
       "dicompyler-core ==0.5.5",
